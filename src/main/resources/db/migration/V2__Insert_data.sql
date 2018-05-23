40f957c63d2fab878decdaade9e72414c4ec9746
LiYanLance <leeyanly@163.com>
2018-05-23 16:22:58 +0800

    test WH7

--- src/main/resources/db/migration/V2__Insert_data.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO Product (id,name,description,price,inventory) VALUES ( 1, 'apple', 'red apple', 10, 100);
-INSERT INTO Product (id,name,description,price,inventory) VALUES ( 2, 'banana', 'yellow banana', 5, 100);
-INSERT INTO Product (id,name,description,price,inventory) VALUES ( 3, 'cigar', 'as it said', 50, 100);
-INSERT INTO Product (id,name,description,price,inventory) VALUES ( 4, 'cocacola', 'cola', 3, 100);
-INSERT INTO Product (id,name,description,price,inventory) VALUES ( 5, 'watermelon', 'juicy', 2, 100);
